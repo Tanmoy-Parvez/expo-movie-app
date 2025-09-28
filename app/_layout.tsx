805e0ffa22bf5afea95d42d7273f3b88acab0632
Tanmoy Parvez <tanmoyparvez23@gmail.com>
2025-09-28 20:37:02 +0600

    add: home page with movie cards

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout() {
         }}
       />
       <Stack.Screen
-        name="movies/[movieId]"
+        name="movie/[movieId]"
         options={{
           headerShown: false,
         }}
