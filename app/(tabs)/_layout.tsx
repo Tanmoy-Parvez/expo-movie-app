25ab6ae02cb1d6220a9db172d2482327cbe2d019
Tanmoy Parvez <tanmoyparvez23@gmail.com>
2025-09-28 18:39:46 +0600

    add: home page bg with logo

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -23,7 +23,7 @@ const TabIcon = ({
     return (
       <ImageBackground
         source={images.highlight}
-        className="flex flex-row w-full flex-1 min-w-[114px] min-h-[54px] mt-4 justify-center items-center rounded-full overflow-hidden"
+        className="flex flex-row w-full flex-1 min-w-[114px] min-h-12 mt-4 justify-center items-center rounded-full overflow-hidden"
       >
         <Image source={icon} tintColor="#151312" className="size-5" />
         <Text className="text-base font-semibold ml-2">{title}</Text>
